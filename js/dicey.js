3cc904a781fd824ccf4356b7f42d43dc2ba19eb5
Kevin McGillivray <kevin.mcgillivray@me.com>
2015-08-05 16:58:58 -0500

    Add roll dice function :game_die:

--- js/dicey.js
+++ js/dicey.js
@@ -5,7 +5,32 @@ function preload() {
 }
 
 function create() {
-  gameTitle = game.add.text(350, 300, 'Dicey', { fontSize: '50px', fill: '#FFF' })
+  gameTitle = game.add.text(350, 300, 'Dicey', { fontSize: '50px', fill: '#FFF' });
+
+  var number;
+  var rolls = [];
+
+  // Generate a random number between 1 and the chosen number of sides
+  function generateNumber(numberOfSides) {
+    number = Math.floor((Math.random() * numberOfSides) + 1);
+    return number;
+  }
+
+  // Save a result to an array
+  function saveResult(array, result) {
+    array.push(result);
+  }
+
+  // Roll a chosen number of X-sided dice
+  function rollDice(numberOfDice, numberOfSides) {
+    for (var i = 0; i < numberOfDice; i++) {
+      generateNumber(numberOfSides);
+      saveResult(rolls, number);
+    }
+  }
+
+  // Rolle six 4-sided dice
+  rollDice(6, 4);
 }
 
 function update() {

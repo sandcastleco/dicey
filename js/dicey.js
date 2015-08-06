cc4e305bcbcbd5799fbeb948a669af1b6fb9ba24
Kevin McGillivray <kevin.mcgillivray@me.com>
2015-08-06 16:51:28 -0500

    Add button :confounded:

--- js/dicey.js
+++ js/dicey.js
@@ -1,50 +1,57 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'dicey', { preload: preload, create: create, update: update });
 
 function preload() {
-
+  game.load.image('button', 'img/button.png');
 }
+
+var button;
+var number;
+var dice;
+var rolls = [];
 
 function create() {
   gameTitle = game.add.text(350, 300, 'Dicey', { fontSize: '50px', fill: '#FFF' });
+  dice = game.add.group();
+  button = game.add.button(game.world.centerX - 50, 400, 'button', function() { rollDice(6, 4)}, this);
+}
 
-  var number;
-  var rolls = [];
+// Generate a random number between 1 and the chosen number of sides
+function generateNumber(numberOfSides) {
+  number = Math.floor((Math.random() * numberOfSides) + 1);
+  return number;
+}
 
-  // Generate a random number between 1 and the chosen number of sides
-  function generateNumber(numberOfSides) {
-    number = Math.floor((Math.random() * numberOfSides) + 1);
-    return number;
+// Save a result to an array
+function saveResult(array, result) {
+  array.push(result);
+}
+
+function showDice(array) {
+  for (var i = 0; i < array.length; i++) {
+    spacing = 800/array.length;
+    height = Math.random() * 400;
+    var die = game.add.text((i * spacing) + 40, height, array[i], { fill: '#FFF' });
+    dice.add(die);
   }
+}
 
-  // Save a result to an array
-  function saveResult(array, result) {
-    array.push(result);
+function clearBoard() {
+  rolls = [];
+  dice.destroy(true, true);
+}
+
+// Roll a chosen number of X-sided dice
+function rollDice(numberOfDice, numberOfSides) {
+  // If there was a previous roll, clear it out
+  if (dice.length > 0) {
+    clearBoard();
   }
-
-  // Roll a chosen number of X-sided dice
-  function rollDice(numberOfDice, numberOfSides) {
-    for (var i = 0; i < numberOfDice; i++) {
-      generateNumber(numberOfSides);
-      saveResult(rolls, number);
-    }
-    showDice(rolls);
+  rolls = [];
+  for (var i = 0; i < numberOfDice; i++) {
+    generateNumber(numberOfSides);
+    saveResult(rolls, number);
   }
-
-  // Rolle six 4-sided dice
-  rollDice(6, 4);
-
-  function showDice(array) {
-    for (var i = 0; i < array.length; i++) {
-      spacing = 800/array.length;
-      height = Math.random() * 400;
-      var die = game.add.text((i * spacing) + 40, height, array[i], { fill: '#FFF' });
-    }
-  }
-
-  // for (var i = 0; i < 12; i++) {
-  //   height = Math.random() * 600;
-  //   var die = game.add.text(i * 70, height, i, {fill: '#FFF' });
-  // }
+  showDice(rolls);
 }
 
 function update() {

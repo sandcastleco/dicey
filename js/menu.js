e9b19518dcdeb805e13d0aadadfbf801a8989a78
Kevin McGillivray <kevin.mcgillivray@me.com>
2015-08-14 12:15:37 -0500

    Make first challenge text clickable

--- js/menu.js
+++ js/menu.js
@@ -7,7 +7,7 @@ var menuState = {
     nameLabel.fontSize = 50;
     nameLabel.fill = '#FFF';
 
-    game.add.text(80, 150, 'Challenge 1', { font: '20px Bree', fill: '#FFF'});
+    var challengeOne = game.add.text(80, 150, 'Challenge 1', { font: '20px Bree', fill: '#FFF'});
     game.add.text(80, 200, 'Challenge 2', { font: '20px Bree', fill: '#FFF'});
     game.add.text(80, 250, 'Challenge 3', { font: '20px Bree', fill: '#FFF'});
     game.add.text(80, 300, 'Challenge 4', { font: '20px Bree', fill: '#FFF'});
@@ -15,9 +15,14 @@ var menuState = {
 
     game.add.text(80, 500, 'Play selected mode!', { font: '30px Bree', fill: '#FFF'});
 
-    nameLabel.inputEnabled = true;
-    nameLabel.input.enableDrag();
+    challengeOne.inputEnabled = true;
 
+    var down = function(item) {
+      game.add.text(300, 150, '> Challenge 1 Info', { font: '20px Bree', fill: '#FFF'});
+    };
+
+    challengeOne.events.onInputDown.add(down, this);
+    
     // var startButton = game.add.button(game.world.centerX - 50, 400, 'button', function() { game.state.start('play'), this });
 
     var music = game.add.audio('theme');

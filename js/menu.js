9004814c03a60c1f25e198577771d889c79abeb0
Kevin McGillivray <kevin.mcgillivray@me.com>
2015-08-14 12:32:58 -0500

    Make play button clickable with child text

--- js/menu.js
+++ js/menu.js
@@ -8,12 +8,15 @@ var menuState = {
     nameLabel.fill = '#FFF';
 
     var challengeOne = game.add.text(80, 150, 'Challenge 1', { font: '20px Bree', fill: '#FFF'});
+
     game.add.text(80, 200, 'Challenge 2', { font: '20px Bree', fill: '#FFF'});
     game.add.text(80, 250, 'Challenge 3', { font: '20px Bree', fill: '#FFF'});
     game.add.text(80, 300, 'Challenge 4', { font: '20px Bree', fill: '#FFF'});
     game.add.text(80, 350, 'Challenge 5', { font: '20px Bree', fill: '#FFF'});
 
-    game.add.text(80, 500, 'Play selected mode!', { font: '30px Bree', fill: '#FFF'});
+    var startButton = game.add.button(80, 500, '', function() { game.state.start('play'), this});
+    var startText = game.add.text(0, 0, 'Play selected mode!', { font: '30px Bree', fill: '#FFF'});
+    startButton.addChild(startText);
 
     challengeOne.inputEnabled = true;
 
@@ -22,7 +25,7 @@ var menuState = {
     };
 
     challengeOne.events.onInputDown.add(down, this);
-    
+
     // var startButton = game.add.button(game.world.centerX - 50, 400, 'button', function() { game.state.start('play'), this });
 
     var music = game.add.audio('theme');

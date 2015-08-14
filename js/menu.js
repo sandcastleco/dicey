996c08bd73a8ac7d1ec56c15b0a1eef496f9f19c
Kevin McGillivray <kevin.mcgillivray@me.com>
2015-08-14 11:55:07 -0500

    Add challenges text to menu, make title draggable

--- js/menu.js
+++ js/menu.js
@@ -7,7 +7,18 @@ var menuState = {
     nameLabel.fontSize = 50;
     nameLabel.fill = '#FFF';
 
-    var startButton = game.add.button(game.world.centerX - 50, 400, 'button', function() { game.state.start('play'), this });
+    game.add.text(80, 150, 'Challenge 1', { font: '20px Bree', fill: '#FFF'});
+    game.add.text(80, 200, 'Challenge 2', { font: '20px Bree', fill: '#FFF'});
+    game.add.text(80, 250, 'Challenge 3', { font: '20px Bree', fill: '#FFF'});
+    game.add.text(80, 300, 'Challenge 4', { font: '20px Bree', fill: '#FFF'});
+    game.add.text(80, 350, 'Challenge 5', { font: '20px Bree', fill: '#FFF'});
+
+    game.add.text(80, 500, 'Play selected mode!', { font: '30px Bree', fill: '#FFF'});
+
+    nameLabel.inputEnabled = true;
+    nameLabel.input.enableDrag();
+
+    // var startButton = game.add.button(game.world.centerX - 50, 400, 'button', function() { game.state.start('play'), this });
 
     var music = game.add.audio('theme');
     music.loop = true;

c1bd409b52c2d89b09308a006f112a4c5514f070
Kevin McGillivray <kevin.mcgillivray@me.com>
2015-08-15 20:18:24 -0500

    Break challenge buttons into separate function

--- js/menu.js
+++ js/menu.js
@@ -2,33 +2,31 @@ var menuState = {
 
   create: function() {
 
-    var nameLabel = game.add.text(game.world.centerX - 60, 50, 'Dicey');
-    nameLabel.font = 'Bree';
-    nameLabel.fontSize = 50;
-    nameLabel.fill = '#FFF';
-
-    challengeInfo = game.add.group();
-    console.log(challengeInfo);
-
     var challenges = game.cache.getJSON('challenges');
     var numberOfChallenges = challenges.length;
+    var challengeInfo = game.add.group();
+
+    var music = game.add.audio('theme', 1, true);
+    music.play();
+
+    var nameLabel = game.add.text(game.world.centerX - 60, 50, 'Dicey', { font: '50px Bree', fill: '#FFF'});
 
     for (i = 0; i < numberOfChallenges; i++) {
       challenge = challenges[i];
-      button = game.add.button(80, ((i+1) * 50) + 100, '', this.showInfo, this);
-      button.challenge = challenge;
-      text = game.add.text(0, 0, challenge.name, { font: '20px Bree', fill: '#FFF'});
-      button.addChild(text);
+      this.createChallengeButton(i, challenge);
     }
 
     var startButton = game.add.button(80, 500, '', this.start, this);
     var startText = game.add.text(0, 0, 'Play selected mode!', { font: '30px Bree', fill: '#FFF'});
     startButton.addChild(startText);
 
-    var music = game.add.audio('theme');
-    music.loop = true;
-    music.play();
+  },
 
+  createChallengeButton: function(index, challenge) {
+    button = game.add.button(80, ((index + 1) * 50) + 100, '', this.showInfo, this);
+    button.challenge = challenge;
+    text = game.add.text(0, 0, challenge.name, { font: '20px Bree', fill: '#FFF'});
+    button.addChild(text);
   },
 
   showInfo: function(item) {

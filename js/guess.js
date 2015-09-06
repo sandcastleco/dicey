5c60ae426d6ea19a15c5b2167ee850adf38ae437
Kevin McGillivray <kevin.mcgillivray@me.com>
2015-09-06 13:00:19 -0500

    Change font

--- js/guess.js
+++ js/guess.js
@@ -1,7 +1,7 @@
 var guessState = {
 
   create: function() {
-    var question = game.add.text(game.world.centerX, 80, selectedChallenge.question, { font: '30px Bree', fill: '#FFF' });
+    var question = game.add.text(game.world.centerX, 80, selectedChallenge.question, { font: '30px museo-sans-rounded', fill: '#FFF' });
     question.anchor.set(0.5);
     alert("The answer is " + total);
   }

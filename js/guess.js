e06dce3e43cb5ce7bec7187c4493c2b00b9a0ff8
Kevin McGillivray <kevin.mcgillivray@me.com>
2015-09-28 16:28:47 -0500

    Add form field to guess

--- js/guess.js
+++ js/guess.js
@@ -4,14 +4,58 @@ var guessState = {
 
     makeBackground();
 
+    var f = document.createElement("form");
+    f.setAttribute('id', 'my-form');
+    f.setAttribute('method',"post");
+    f.setAttribute('action',"submit.php");
+
+    var i = document.createElement("input"); //input element, text
+    i.setAttribute('id', 'my-input')
+    i.setAttribute('type',"text");
+    i.setAttribute('name',"username");
+
+    var s = document.createElement("input"); //input element, Submit button
+    s.setAttribute('type',"submit");
+    s.setAttribute('value',"Submit");
+
+    f.appendChild(i);
+    f.appendChild(s);
+
+    //and some more input elements here
+    //and dont forget to add a submit button
+
+    document.getElementsByTagName('body')[0].appendChild(f);
+
+    function processForm(e) {
+      if (e.preventDefault) e.preventDefault();
+        guess = document.getElementById('my-input').value;
+        if (guess == total) {
+          alert("Correct! You win!");
+        } else {
+          alert("Bzzzzt, guess again...");
+        }
+
+      /* do what you want with the form */
+
+      // You must return false to prevent the default form behavior
+      return false;
+    }
+
+    var form = document.getElementById('my-form');
+    if (form.attachEvent) {
+      form.attachEvent("submit", processForm);
+    } else {
+      form.addEventListener("submit", processForm);
+    }
+
     var question = game.add.text(game.world.centerX, 80, selectedChallenge.question, { font: '30px museo-sans-rounded', fill: '#808181' });
     question.anchor.set(0.5);
 
-    var points = [[1,1],[2,1],[3,1],[4,1],[5,1],[1,2],[2,2],[3,2],[4,2],[5,2]];
+    //var points = [[1,1],[2,1],[3,1],[4,1],[5,1],[1,2],[2,2],[3,2],[4,2],[5,2]];
 
-    for (i = 0; i < points.length; i++) {
-      text = game.add.text((game.world.centerX - 150) + points[i][0] * 40, 200 + [points[i][1]] * 40, '•', { font: '40px museo-sans-rounded', fill: '#808181'});
-    }
+    //for (i = 0; i < points.length; i++) {
+    //  text = game.add.text((game.world.centerX - 150) + points[i][0] * 40, 200 + [points[i][1]] * 40, '•', { font: '40px museo-sans-rounded', fill: '#808181'});
+    //}
   }
-  
+
 };

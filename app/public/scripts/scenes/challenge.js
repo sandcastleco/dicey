e0b57cde446cc480c66deec1f4803cbdc43e2cb4
Kevin McGillivray <kevin.mcgillivray@me.com>
2017-02-12 22:22:06 -0600

    Add front end level looping

--- app/public/scripts/scenes/challenge.js
+++ app/public/scripts/scenes/challenge.js
@@ -1,16 +1,16 @@
 var challengeScene = new Scene();
 
 challengeScene.init = function(opts) {
-  var level = opts.level - 1;
-  var challenge = opts.challenge - 1;
-  this.currentLevel = level + 1;
-  this.currentChallenge = challenge + 1;
-  this.challengeData = game.levels[level].challenges[challenge];
+  game.currentLevel = opts.level;
+  game.currentChallenge = opts.challenge;
+  var levelIndex = opts.level - 1;
+  var challengeIndex = opts.challenge - 1;
+  this.challengeData = game.levels[levelIndex].challenges[challengeIndex];
 }
 
 challengeScene.rollDice = function(cb) {
   var thisChallenge = this;
-  var apiStr = 'api/level/' + this.currentLevel + '/' + this.currentChallenge;
+  var apiStr = 'api/level/' + game.currentLevel + '/' + game.currentChallenge;
   var xhr = new XMLHttpRequest();
   xhr.open('GET', apiStr);
   xhr.setRequestHeader('Content-Type', 'application/json');
@@ -34,7 +34,7 @@ challengeScene.draw = function() {
   game.element.innerHTML += "<h2>Play!</h2>";
   this.rollDice(function(results) {
     for (var i = 0; i < results.length; i++) {
-      game.element.innerHTML += results[i].result;
+      game.element.innerHTML += results[i].result + ' ';
     }
     game.element.innerHTML += "<p id='timer'></p>";
     scene.updateTimer(time);
--- app/public/scripts/scenes/result.js
+++ app/public/scripts/scenes/result.js
@@ -8,8 +8,42 @@ resultScene.draw = function() {
   game.element.innerHTML += "<h2>Result!</h2>";
   if (this.win) {
     game.element.innerHTML += "<h3>Correct!</h3>";
+    var level = game.levels[game.currentLevel - 1];
+    if (game.currentLevel == game.levels.length && game.currentChallenge == level.challenges.length) {
+      game.element.innerHTML += "<p>End of challenges!</p>";
+    } else {
+      game.element.innerHTML += "<button id='next'>Next challenge</button>";
+    }
   } else {
     game.element.innerHTML += "<h3>Nope...</h3>";
+    game.element.innerHTML += "<button id='next'>Try again</button>";
   }
   document.cookie = "diceyRes=null";
+
+  var nextButton = document.getElementById("next");
+  nextButton.addEventListener("click", function(e) {
+    console.log('test');
+    e.preventDefault();
+    var level = game.levels[game.currentLevel - 1];
+    var nextLevel = game.currentLevel;
+    var nextChallenge = game.currentChallenge;
+    if (resultScene.win) {
+      if (game.currentChallenge == level.challenges.length) {
+        if (game.currentLevel == game.levels.length) {
+          game.setScene(menu);
+        } else {
+          nextLevel = parseInt(game.currentLevel) + 1;
+          nextChallenge = 1;
+        }
+      } else {
+        nextLevel = game.currentLevel;
+        nextChallenge = parseInt(game.currentChallenge) + 1;
+      }
+    }
+
+    game.setScene(challengeScene, {
+      level: nextLevel,
+      challenge: nextChallenge
+    });
+  })
 }

5e9b2043799879049afe9f0eff70b6ebefeec64a
Kevin McGillivray <kevin.mcgillivray@me.com>
2017-02-12 19:47:20 -0600

    Build backend challenge flow

--- app/server.js
+++ app/server.js
@@ -1,4 +1,6 @@
 var express = require('express')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
 var nunjucks = require('nunjucks')
 var app = express()
 var shuffle = require('./modules/shuffle').shuffle
@@ -10,6 +12,9 @@ nunjucks.configure('app', {
 })
 
 app.use(express.static(__dirname + '/public'));
+app.use(cookieParser('asecretpassword'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 function rollDice(dice) {
   var results = [];
@@ -40,6 +45,22 @@ app.get('/level/:levelId/:challengeId', function (req, res) {
   var level = levels[req.params.levelId - 1];
   var challenge = level.challenges[req.params.challengeId - 1];
   res.locals.results = rollDice(challenge.parameters.dice);
+
+  var total = (function() {
+    var sum = 0;
+    for (var i = 0; i < res.locals.results.length; i++) {
+      var result = res.locals.results[i];
+      sum += result.result;
+    }
+    return sum;
+  })();
+
+  var options = {
+      maxAge: 1000 * 60 * 15,
+      signed: true
+  }
+
+  res.cookie('diceyRes', total, options);
   res.locals.refresh = {
     time: challenge.parameters.time,
     location: '/level/' + req.params.levelId + '/' + req.params.challengeId + '/guess'
@@ -48,10 +69,23 @@ app.get('/level/:levelId/:challengeId', function (req, res) {
 })
 
 app.get('/level/:levelId/:challengeId/guess', function (req, res) {
+  res.locals.levelId = req.params.levelId;
+  res.locals.challengeId = req.params.challengeId;
   res.render('views/guess.html')
 })
 
 app.get('/level/:levelId/:challengeId/result', function (req, res) {
+  var answer = req.signedCookies.diceyRes;
+  res.locals.win = false;
+  if (req.query.answer == answer) {
+    res.locals.win = true;
+  }
+  var options = {
+      maxAge: 1000 * 60 * 15,
+      signed: true
+  }
+  res.cookie('diceyRes', null, options);
+  console.log(req.signedCookies);
   res.render('views/result.html')
 })
 
